ad12f2bb4e827d9ff92f3ad3c565d8ed9391acb8
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-01-24 11:39:02 -0300

    feat: project adjustments and create module login

---
+++ react-frontend/src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgroundImage src="./background.svg" />
+    </div>
+  );
+};
+
+export default LoginScreen;
