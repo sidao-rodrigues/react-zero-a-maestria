e0d21409f4930f2367238271e63b8c815cdd963d
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-01-24 15:06:16 -0300

    feat: insert state in login

--- react-frontend/src/shared/inputs/input/input.tsx
+++ react-frontend/src/shared/inputs/input/input.tsx
@@ -6,11 +6,12 @@ interface InputProps extends InputPropsAntd {
   //InputHTMLAttributes<HTMLInputElement> {
   //extende todas as funções do component input do html (existe dessa forma comentada)
   title?: string;
+  margin?: string;
 }
 
-const Input = ({ title, ...props }: InputProps) => {
+const Input = ({ title, margin, ...props }: InputProps) => {
   return (
-    <BoxInput>
+    <BoxInput style={{ margin }}>
       {title && <TitleInput>{title}</TitleInput>}
       <InputAntd {...props} />
     </BoxInput>
