29796206af8875aff96dd2bbbd5d2cec8fb2c717
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-01-24 12:19:02 -0300

    feat: create Login component

--- react-frontend/src/modules/login/screens/LoginScreen.tsx
+++ react-frontend/src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,22 @@
-import { BackgroundImage } from '../styles/loginScreen.styles';
+import {
+  BackgroundImage,
+  ContainerLogin,
+  ContainerLoginScreen,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
-      <BackgroundImage src="./background.svg" />
+      <ContainerLoginScreen>
+        <ContainerLogin>
+          <LimitedContainer>
+            <LogoImage src="./logo.png" />
+          </LimitedContainer>
+        </ContainerLogin>
+        <BackgroundImage src="./background.png" />
+      </ContainerLoginScreen>
     </div>
   );
 };
