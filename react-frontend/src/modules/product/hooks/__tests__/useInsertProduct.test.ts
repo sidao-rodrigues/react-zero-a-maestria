484911cdf95a9ade5d260883905ccb95274c7542
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-02-14 19:09:34 -0300

    feat: test useEffect in hooks

--- react-frontend/src/modules/product/hooks/__tests__/useInsertProduct.test.ts
+++ react-frontend/src/modules/product/hooks/__tests__/useInsertProduct.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { act, renderHook } from '@testing-library/react';
 
 import { useInsertProduct } from '../useInsertProduct';
@@ -55,10 +56,45 @@ describe('Test userInsertProduct', () => {
     const { result } = renderHook(() => useInsertProduct());
 
     act(() => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       result.current.handleOnChangeInput({ target: { value: TEST_MOCK } } as any, 'price', true);
     });
 
     expect(result.current.product.price).toEqual(Number(TEST_MOCK));
+  });
+
+  it('should change disabledButton in insert data', () => {
+    const { result } = renderHook(() => useInsertProduct());
+
+    expect(result.current.disabledButton).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({ target: { value: '4321' } } as any, 'price', true);
+    });
+
+    expect(result.current.disabledButton).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({ target: { value: 'zezo' } } as any, 'name');
+    });
+
+    expect(result.current.disabledButton).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeInput({ target: { value: 'http://img' } } as any, 'image');
+    });
+
+    expect(result.current.disabledButton).toEqual(true);
+
+    act(() => {
+      result.current.handleOnChangeSelect('4312');
+    });
+
+    expect(result.current.disabledButton).toEqual(false);
+
+    act(() => {
+      result.current.handleOnChangeInput({ target: { value: '' } } as any, 'image');
+    });
+
+    expect(result.current.disabledButton).toEqual(true);
   });
 });
