0d5811d341a68a2b8f5cac1a595423d2396bb31a
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-02-14 19:01:17 -0300

    feat: insert unit test in useInsertProduct

---
+++ react-frontend/src/modules/product/hooks/__tests__/useInsertProduct.test.ts
@@ -0,0 +1,64 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { useInsertProduct } from '../useInsertProduct';
+
+const mockNavigate = jest.fn();
+const mockSetNotification = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  useNavigate: () => mockNavigate,
+}));
+
+jest.mock('../../../../store/reducers/globalReducer/useGlobalReducer', () => ({
+  useGlobalReducer: () => ({
+    setNotification: mockSetNotification,
+  }),
+}));
+
+describe('Test userInsertProduct', () => {
+  it('should return inital values', () => {
+    const { result } = renderHook(() => useInsertProduct());
+
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.disabledButton).toEqual(true);
+    expect(result.current.product).toEqual({
+      name: '',
+      price: 0,
+      image: '',
+    });
+  });
+
+  it('should change select in handleOnChangeSelect', () => {
+    const { result } = renderHook(() => useInsertProduct());
+
+    act(() => {
+      result.current.handleOnChangeSelect('4312');
+    });
+
+    expect(result.current.product.categoryId).toEqual(4312);
+  });
+
+  it('should change product in handleOnChangeInput send name', () => {
+    const TEST_MOCK = 'TEST_MOCK';
+    const { result } = renderHook(() => useInsertProduct());
+
+    act(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      result.current.handleOnChangeInput({ target: { value: TEST_MOCK } } as any, 'name');
+    });
+
+    expect(result.current.product.name).toEqual(TEST_MOCK);
+  });
+
+  it('should change product in handleOnChangeInput send price', () => {
+    const TEST_MOCK = '3453';
+    const { result } = renderHook(() => useInsertProduct());
+
+    act(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      result.current.handleOnChangeInput({ target: { value: TEST_MOCK } } as any, 'price', true);
+    });
+
+    expect(result.current.product.price).toEqual(Number(TEST_MOCK));
+  });
+});
