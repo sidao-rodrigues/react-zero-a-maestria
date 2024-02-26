c2fe78cec6fcce15ba58b85397d6484f4144daa8
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-02-26 15:04:36 -0300

    feat: insert table orders

--- react-frontend/src/modules/orders/screen/Order.tsx
+++ react-frontend/src/modules/orders/screen/Order.tsx
@@ -1,14 +1,53 @@
+import { ColumnsType } from 'antd/es/table';
+
+import { IListBreadcrumb } from '../../../shared/components/breadcrumb/Breadcrumb';
 import Screen from '../../../shared/components/screen/Screen';
+import Table from '../../../shared/components/table/Table';
+import { IOrderType } from '../../../shared/types/OrderType';
 import { useOrder } from '../hooks/useOrder';
+
+const columns: ColumnsType<IOrderType> = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Data',
+    dataIndex: 'date',
+    key: 'date',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Usuário',
+    dataIndex: 'user',
+    key: 'user',
+    render: (_, target) => <a>{target.user?.name}</a>,
+  },
+  {
+    title: 'Qtd. Produtos',
+    dataIndex: 'amountProducts',
+    key: 'amountProducts',
+    render: (text: number) => <a>{text}</a>,
+  },
+];
 
 const Order: React.FC = () => {
   const { orders } = useOrder();
+
+  const listBreadcrumb: IListBreadcrumb[] = [
+    {
+      name: 'HOME',
+    },
+    {
+      name: 'PEDIDOS',
+    },
+  ];
+
   return (
-    <Screen>
-      {/* {orders.map((order) => (
-        <div key={order.id}>{order.userId}</div>
-      ))} */}
-      <div>Order</div>
+    <Screen listBrandcrumb={listBreadcrumb}>
+      <Table columns={columns} dataSource={orders} rowKey="id" />
     </Screen>
   );
 };
