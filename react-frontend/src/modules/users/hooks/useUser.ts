55b83795224332756663bf5f55909695cd368d97
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-02-27 11:43:04 -0300

    feat: insert table in user screen

--- react-frontend/src/modules/users/hooks/useUser.ts
+++ react-frontend/src/modules/users/hooks/useUser.ts
@@ -1,20 +1,36 @@
-import { useEffect } from 'react';
+import { SearchProps } from 'antd/es/input';
+import { useEffect, useState } from 'react';
 
 import { URL_USER_ALL } from '../../../shared/constants/urls';
 import { EMethodsEnum } from '../../../shared/enums/methods.enum';
 import { useRequests } from '../../../shared/hooks/useRequest';
 import { useUserReducer } from '../../../store/reducers/userReducer/useUserReducer';
+import { IUserType } from '../../login/types/UserType';
 
 export const useUser = () => {
   const { users, setUsers } = useUserReducer();
+  const [usersFiltered, setUsersFiltered] = useState<IUserType[]>([]);
   const { request, loading } = useRequests();
 
   useEffect(() => {
     request(URL_USER_ALL, EMethodsEnum.GET, setUsers);
   }, []);
 
+  useEffect(() => {
+    setUsersFiltered([...users]);
+  }, [users]);
+
+  const handleOnChangeSearch: SearchProps['onSearch'] = (value: string) => {
+    if (!value) {
+      setUsersFiltered(users);
+    } else {
+      setUsersFiltered([...users.filter((user) => new RegExp(value, 'i').test(user.name))]);
+    }
+  };
+
   return {
-    users,
+    users: usersFiltered,
     loading,
+    handleOnChangeSearch,
   };
 };
--- react-frontend/src/modules/users/screens/User.tsx
+++ react-frontend/src/modules/users/screens/User.tsx
@@ -1,11 +1,89 @@
+import { Input, Spin } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+
+import { IListBreadcrumb } from '../../../shared/components/breadcrumb/Breadcrumb';
+import Button from '../../../shared/components/buttons/button/button';
 import Screen from '../../../shared/components/screen/Screen';
+import {
+  DisplayFlexJustifyBetween,
+  DisplayFlexJustifyCenter,
+} from '../../../shared/components/styles/display.styles';
+import { LimitedContainer } from '../../../shared/components/styles/limited.styles';
+import Table from '../../../shared/components/table/Table';
+import { insertMaskInCpf } from '../../../shared/functions/cpf';
+import { insertMaskInPhone } from '../../../shared/functions/phone';
+import { IUserType } from '../../login/types/UserType';
 import { useUser } from '../hooks/useUser';
 
+const { Search } = Input;
+
+const columns: ColumnsType<IUserType> = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Nome',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    render: (text: string) => <a>{text}</a>,
+  },
+  {
+    title: 'Telefone',
+    dataIndex: 'phone',
+    key: 'phone',
+    render: (text: string) => <a>{insertMaskInPhone(text)}</a>,
+  },
+  {
+    title: 'CPF',
+    dataIndex: 'cpf',
+    key: 'cpf',
+    render: (text: string) => <a>{insertMaskInCpf(text)}</a>,
+  },
+];
+
 const User: React.FC = () => {
-  const { users } = useUser();
+  const { users, loading, handleOnChangeSearch } = useUser();
+
+  const listBreadcrumb: IListBreadcrumb[] = [
+    {
+      name: 'HOME',
+    },
+    {
+      name: 'USUÁRIOS',
+    },
+  ];
+
   return (
-    <Screen>
-      <div>Users</div>
+    <Screen listBrandcrumb={listBreadcrumb}>
+      {loading ? (
+        <DisplayFlexJustifyCenter>
+          <Spin size="large" />
+        </DisplayFlexJustifyCenter>
+      ) : (
+        <>
+          <DisplayFlexJustifyBetween margin="0px 0px 16px 0px">
+            <LimitedContainer width={240}>
+              <Search placeholder="Buscar Usuário" onSearch={handleOnChangeSearch} enterButton />
+            </LimitedContainer>
+
+            <LimitedContainer width={120}>
+              <Button type="primary" onClick={() => null}>
+                Inserir
+              </Button>
+            </LimitedContainer>
+          </DisplayFlexJustifyBetween>
+          <Table columns={columns} dataSource={users} rowKey="id" />
+        </>
+      )}
     </Screen>
   );
 };
